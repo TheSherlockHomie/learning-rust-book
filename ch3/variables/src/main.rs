cbfcbb4ce8b96081901119ec24474d1ad1c663c6
Kushagra Kalash <kushagrakdev@gmail.com>
2021-07-20 09:28:46 +0530

    Rust Ch3 Scalar Types

---
+++ ch3/variables/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    let x = 1;
+    let y = 2;
+
+    println!("The sum of {} and {} is {}", x, y, x+y);
+
+    let x: f32 = 1.1;
+    let y: f32 = 2.1;
+
+    println!("The sum of {} and {} is {}", x, y, x+y);
+
+    let x = true;
+    println!("the value of x is {}", x);
+
+    let c = '😁';
+    println!("c is an emoji! {}", c);
+}
